ed55c18bd5b1ee56d677073bd3aa4ffdaed4b5c6
lucas-catto <lucascattosilva2018@gmail.com>
2024-06-05 08:51:15 -0300

    Criando o Aplicativo e conectando com o Firebase.

---
+++ GalloFlix/firebase.config.example.js
@@ -0,0 +1,26 @@
+
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAuth } from 'firebase/auth';
+import { getFirestore } from "firebase/firestore";
+
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
+
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+// Your web app's Firebase configuration
+
+const firebaseConfig = {
+
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+// Initialize Firebase
+const firebase = initializeApp(firebaseConfig);
+const db = getFirestore(firebase);
+const auth = getAuth(firebase);
+
+export { db, auth };
